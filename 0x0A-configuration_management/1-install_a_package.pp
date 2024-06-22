ec34e56ce64ddc9bb8024f29dca2d560652a5bd7
abdullahhr10 <abdullahh.ragab10@gmail.com>
2024-06-21 23:39:38 -0700

    Add Puppet script that installs flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # Installs flask from pip3 using Puppet
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+package { 'flash':
+  ensure   => 2.1.0,
+  provider => 'pip3,
 }
