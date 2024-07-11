f749ff811a1715d4fc474e73cd44e0163a9d5588
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-07-11 07:40:02 -0700

    Add Puppet script that transfers a file from our client to a server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,53 +1,23 @@
-# Installs Nginx on web-01
-node 'web-01' {
-  
-  # Ensure the package lists are up to date and install Nginx
-  exec { 'update_package_list':
-    command => '/usr/bin/apt-get update -y -qq',
-    path    => ['/usr/bin', '/usr/sbin'],
-  }
+# Installs Nginx
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
-  package { 'nginx':
-    ensure  => installed,
-    require => Exec['update_package_list'],
-  }
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
 
-  # Allow Nginx on the firewall
-  exec { 'allow_nginx_firewall':
-    command => '/usr/sbin/ufw allow \'Nginx HTTP\'',
-    path    => ['/usr/bin', '/usr/sbin'],
-  }
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
 
-  # Give the user ownership of website files for easy editing
-  file { '/var/www/html':
-    ensure  => directory,
-    owner   => $::user,
-    group   => $::user,
-    mode    => '0755',
-    recurse => true,
-    require => Package['nginx'],
-  }
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
 
-  # Backup default index.html
-  file { '/var/www/html/index.nginx-debian.html.bckp':
-    ensure => file,
-    source => '/var/www/html/index.nginx-debian.html',
-    require => Package['nginx'],
-  }
-
-  # Create index.html that contains "Hello World!"
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    mode    => '0644',
-    require => Package['nginx'],
-  }
-
-  # Ensure Nginx service is running
-  service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    subscribe  => File['/var/www/html/index.html'],
-  }
-
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
