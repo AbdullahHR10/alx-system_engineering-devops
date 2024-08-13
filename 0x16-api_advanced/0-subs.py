188976a08d4046eacaacd83ce22edc6c8fdfb91d
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-13 04:54:45 -0700

    Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,12 @@ import requests
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-    sub_info = requests.get(
-        "https://www.reddit.com/r/{subreddit}/about.json",
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False,
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
     )
-    if sub_info.status_code >= 300:
-        return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
