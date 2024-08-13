b72793198f78940a19873087a2b7cbcf01eda3d7
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-13 04:50:32 -0700

    Reformatted

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,11 @@ import requests
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-    sub_info = requests.get("https://www.reddit.com/r/{subreddit}/about.json",
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    sub_info = requests.get(
+        "https://www.reddit.com/r/{subreddit}/about.json",
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False,
+    )
     if sub_info.status_code >= 300:
         return 0
 
