63f04143f6009a176dc4d78c65b874e9274b0785
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-14 03:33:43 -0700

    Add task 0 script

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Puppet script that fixes a 500 error
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
